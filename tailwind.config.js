aec20514a8938e6fcdc990f3c190f1a5053a0aae
JcDiarios <jcarlodiarios@gmail.com>
2023-11-20 21:15:44 +0800

    updated tailwind.js added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customPurple: '#4e1a59',
+        customPink: '#c626bf',
+        customGreen: '#97a294',
+        customDarkPurple: '#5a35a4',
+        customDark: '#190f23',
+        customBlue: '#1d4ed8',
+        customLightGreen: '#22e5c6',
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
