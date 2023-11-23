fc218c3a483a873dd255a56c85c8b9f5c07ac513
JcDiarios <jcarlodiarios@gmail.com>
2023-11-23 09:38:38 +0800

    added homepage, footer,  faq design not finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import React, { useEffect } from 'react';
+import GSpaceLogo from "../assets/LOGO.png";
+
 
 export default function NavBar () {
     useEffect(() => {
@@ -57,10 +59,9 @@ export default function NavBar () {
 
     return (
         <>
-            <nav className="relative px-4 py-4 flex justify-between items-center bg-transparent border-b-2 border-b-slate-300">
-                <a className="text-3xl font-bold leading-none" href="#">
-                    {/*PENDING LOGO*/}
-                    G-Space
+            <nav className="absolute z-10 px-4 py-4 flex justify-between items-center backdrop-blur-sm bg-transparent border-b-2 border-b-slate-300 w-full">
+                <a className="text-3xl font-bold leading-none w-10" href="#">
+                    <img src={GSpaceLogo} alt="" className="nav-log ml-10" />
                 </a>
                 <div className="lg:hidden">
                     <button className="navbar-burger flex items-center text-blue-600 p-3">
@@ -70,23 +71,22 @@ export default function NavBar () {
                         </svg>
                     </button>
                 </div>
-                <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6 ">
-                    <li className="text-sm text-gray-400 hover:text-gray-500"><Link to="/">Home</Link></li>
-                    <li className="text-sm text-gray-400 hover:text-gray-500"><Link to="/booking">Book Now</Link></li>
-                    <li className="text-sm text-gray-400 hover:text-gray-500"><Link to="/gallery">Gallery</Link></li>
-                    <li className="text-sm text-gray-400 hover:text-gray-500"><Link to="/reviews">Reviews</Link></li>
-                    <li className="text-sm text-gray-400 hover:text-gray-500"><a href="Footer">Contact Us</a></li>
-                    <li className="text-sm text-gray-400 hover:text-gray-500"><Link to="/faq">FAQ</Link></li>
+                <ul className="hidden flex lg:flex lg:items-center lg:space-x-10 ml-96">
+                    <li className="text-sm text-white"><Link to="/">Home</Link></li>
+                    <li className="text-sm text-white"><Link to="/gallery">Gallery</Link></li>
+                    <li className="text-sm text-white"><Link to="/reviews">Reviews</Link></li>
+                    <li className="text-sm text-white"><a href="Footer">Contact Us</a></li>
+                    <li className="text-sm text-white"><Link to="/faq">FAQ</Link></li>
                 </ul>
-                <a className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Log In</a>
-                <a className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="#">Register</a>
+                <a className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200 bg-gradient-to-r from-customPurple to-customPink text-white" href="#">Log In</a>
+                <a className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200 bg-gradient-to-r from-customPurple to-customPink text-white mr-10" href="#">Register</a>
             </nav>
             <div className="navbar-menu relative z-50 hidden">
                 <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
                 <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-gradient-to-br from-violet-800 to-teal-400 border-r overflow-y-auto">
                     <div className="flex items-center mb-8">
                         <a className="mr-auto text-3xl font-bold leading-none" href="#">
-                            G-Space
+                            <img src={GSpaceLogo} alt="" className="nav-logo" />
                         </a>
                         <button className="navbar-close">
                             <svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -96,11 +96,8 @@ export default function NavBar () {
                     </div>
                     <div>
                         <ul>
-                            <li className="mb-1 block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded">
+                            <li className="mb-1 block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-900 rounded">
                                 <Link to="/">Home</Link>
-                            </li>
-                            <li className="mb-1 block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded">
-                                <Link to="/booking">Book Now</Link>
                             </li>
                             <li className="mb-1 block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded">
                                 <Link to="/gallery">Gallery</Link>
@@ -118,7 +115,7 @@ export default function NavBar () {
                     </div>
                     <div className="mt-auto">
                         <div className="pt-6">
-                            <a className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded" href="#">Sign in</a>
+                            <a className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded " href="#">Sign in</a>
                             <a className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700 rounded" href="#">Sign Up</a>
                         </div>
                         <p className="my-4 text-xs text-center text-white">
--- src/components/pages/FAQ.jsx
+++ src/components/pages/FAQ.jsx
@@ -20,7 +20,22 @@ const ArcadeFAQ = () => {
       question: 'Do you offer group discounts for gaming hub rentals?',
       answer: 'Yes, we offer group discounts for gaming hub rentals. Contact our customer service for more information on group rates and special packages.'
     },
-    
+    {
+      question: "Do you offer exclusive access to limited-edition game content?",
+      answer: "Yes, we collaborate with game developers to provide exclusive access to limited-edition game content or early releases. Stay updated on our latest offerings!."
+    },
+    {
+      question: "Can I book the gaming rooms for group events or parties?",
+      answer: "Absolutely! Our spacious gaming rooms are perfect for hosting group events, birthday parties, or gaming gatherings. Contact us for special group booking options."
+    },
+    {
+      question: "Are there themed gaming rooms available?",
+      answer: "Yes, we offer an array of themed rooms inspired by popular games or eras. Immerse yourself in themed environments designed to elevate your gaming experience."
+    },
+    {
+      question: "Can I bring my own gaming equipment?",
+      answer: "Of course! Feel free to bring your gaming gear. However, we also provide high-quality gaming equipment for your convenience."
+    },
   ];
 
   const [activeIndex, setActiveIndex] = useState(null);
@@ -32,30 +47,28 @@ const ArcadeFAQ = () => {
   return (
     <>
     <NavBar/>
-        <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply h-100px">
-     <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">G-Space FAQ`s</h1>
-     </div>
-    </section>
-    <div className="container mx-auto mt-8 p-4">
+    <section className="bg-black pt-40">
+    <div className="container mx-auto p-4 mb-20">
       {faqs.map((faq, index) => (
         <div key={index} className="mb-4">
           <div
-            className="flex items-center justify-between bg-white p-4 rounded shadow cursor-pointer"
+            className="flex items-center justify-between bg-transparent border m-5 p-2 rounded shadow cursor-pointer"
             onClick={() => toggleFAQ(index)}
           >
-            <h3 className="text-lg font-semibold">{faq.question}</h3>
-            <span>{activeIndex === index ? '▲' : '▼'}</span>
+            <h3 className="faq-questions mb-2 text-xl font-bold text-customDarkPurple">{faq.question}</h3>
+            <span className="text-white">{activeIndex === index ? '▼' : '▼'}</span>
           </div>
           {activeIndex === index && (
-            <div className="bg-white p-4 rounded shadow mt-2">
-              <p className="text-gray-800">{faq.answer}</p>
+            <div className="bg-black rounded shadow mt-2">
+              <p className="text-lg ml-5 font-normal text-white">{faq.answer}</p>
             </div>
           )}
         </div>
       ))}
     </div>
     <Footer/>
+    </section>
+
     </>
   );
 };
